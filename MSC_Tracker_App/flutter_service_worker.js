487cb911ad4fcd8ad9849a3e6efff5268f0acd29
Zhiheng Jiang <41467174+jzh001@users.noreply.github.com>
2022-12-30 17:08:23 +0800

    Add files via upload

--- MSC_Tracker_App/flutter_service_worker.js
+++ MSC_Tracker_App/flutter_service_worker.js
@@ -20,11 +20,11 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "119d1d3457a11252edfe4e7fd91ad92c",
-"/": "119d1d3457a11252edfe4e7fd91ad92c",
-"main.dart.js": "803d672c4cb3b708cc9e5d3dae89203c",
+"index.html": "3faf9ab613c4d0ca73bc73d5ce1ab5de",
+"/": "3faf9ab613c4d0ca73bc73d5ce1ab5de",
+"main.dart.js": "8f1602a5a29027cd4c63db2405da3f2e",
 "manifest.json": "8edf6522ed100853b8b5013e6ad4cb47",
-"version.json": "dce41207731cfe00081824f535af4562"
+"version.json": "bc59f1a6b4aeefd4bd0ec826deb7ceeb"
 };
 
 // The application shell files that are downloaded before a service worker can
