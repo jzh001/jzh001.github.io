42476b53d9ef324a440a7dffe15684933117185d
Zhiheng Jiang <41467174+jzh001@users.noreply.github.com>
2023-01-06 20:44:53 +0800

    Add files via upload

--- MSC_Tracker_App/flutter_service_worker.js
+++ MSC_Tracker_App/flutter_service_worker.js
@@ -20,9 +20,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "6d5833bafccfaa9d7d25bf94ffd638f2",
-"/": "6d5833bafccfaa9d7d25bf94ffd638f2",
-"main.dart.js": "d8db0a0051ab61e5367b223f8e7a3d9d",
+"index.html": "ee785152be20358542698a81e25da39a",
+"/": "ee785152be20358542698a81e25da39a",
+"main.dart.js": "284aba903152511188da32ac461e19d6",
 "manifest.json": "8edf6522ed100853b8b5013e6ad4cb47",
 "version.json": "8b555637b8f764395d33d0e636f8e108"
 };
