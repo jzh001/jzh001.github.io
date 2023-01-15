a4155dd93ace44a38331b317988963c7a310a776
Zhiheng Jiang <41467174+jzh001@users.noreply.github.com>
2023-01-15 15:31:40 +0800

    Add files via upload

--- MSC_Tracker_App/flutter_service_worker.js
+++ MSC_Tracker_App/flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "assets/assets/images/MSC_Logo.png": "21d1f57914de4c513fc970e2f24b544e",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
 "assets/fonts/MaterialIcons-Regular.otf": "95db9098c58fd6db106f1116bae85a0b",
-"assets/NOTICES": "737915944fb3e7d46a6fca3cd62499f2",
+"assets/NOTICES": "52e3affed2fe261e42632f5375216218",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/empty_widget/assets/images/emptyImage.png": "6bb2d2c61bb39c0c571b95ae009acc4b",
 "assets/packages/empty_widget/assets/images/im_emptyIcon_1.png": "545e13e1cabb2faf7eac9801f64f7f89",
@@ -24,9 +24,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "265e92d82959f4f58b55104ff2efe55b",
-"/": "265e92d82959f4f58b55104ff2efe55b",
-"main.dart.js": "06e8678fc989173652dbb7921d2f3bda",
+"index.html": "1dd31edc03a0c9f25ace7a04bb0f3cb2",
+"/": "1dd31edc03a0c9f25ace7a04bb0f3cb2",
+"main.dart.js": "567ba1546621fd5e33aa48012e51ba46",
 "manifest.json": "8edf6522ed100853b8b5013e6ad4cb47",
 "version.json": "cae5a9c7bcfa683932e0bbd1e78826c0"
 };
